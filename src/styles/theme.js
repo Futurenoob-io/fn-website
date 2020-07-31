36bc4e2a7ed6f5a8e9360bbab06f19e2df2d4845
vippriya <vippriya@gmail.com>
2020-07-31 23:54:13 +0200

    rv-client footer theme shall be updated

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import { generalBackground } from './colors';
+
+const footer = {
+	background: generalBackground.footerPrimary,
+	padding: '60px',
+	color: 'white',
+	display: 'flex',
+	justify: 'center'
+};
+
+const theme = {
+	footer
+};
+export default theme;
