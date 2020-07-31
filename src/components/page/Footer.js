4b9f9222a1a5ecfd8924bbf7aa2f3389a6633792
vippriya <vippriya@gmail.com>
2020-08-01 00:14:54 +0200

    fn-website updated styles

--- src/components/page/Footer.js
+++ src/components/page/Footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import CopyRights from './CopyRights';
+import theme from '../../styles/theme';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+	footer: theme.footer,
+	logo: {},
+	section: {
+		display: 'flex',
+		justifyContent: 'center',
+		margin: '5px'
+	},
+	signUpSection: {
+		flexGrow: '1',
+		display: 'flex',
+		justifyContent: 'center'
+	}
+});
 
 function Footer(props) {
+	const classes = useStyles();
 	return (
-		<footer>
-			<CopyRights />
+		<footer className={classes.footer}>
+			<div className={classes.logo}>Logo</div>
+			<div className={classes.section}>Company</div>
+			<div className={classes.section}>Legal</div>
+			<div className={classes.section}>Social</div>
+			<div className={classes.signUpSection}>Sign up</div>
 		</footer>
 	);
 }
