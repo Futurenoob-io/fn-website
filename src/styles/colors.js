3a917189b5a3af2401b13305786b81b1329865b7
vippriya <vippriya@gmail.com>
2020-07-31 23:53:46 +0200

    rv client home page background colors updated

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+const generalBackground = {
+	footerPrimary: '#000000',
+	headerPrimary: '#F4F4F5',
+	buttonPrimary: '#F4F4F5',
+	buttonSecondary: '#FF9643'
+};
+
+const homePageBackGround = {
+	homeMainMediaCards: [ '#EBEBEB', '#CECBDA', '#E1EFFA', '#CECBDA', '#FF9643' ],
+	IntegrationCardIcon: '#FFFFFF',
+	IntegrationCardContent: '#E1EFFA',
+	earlyAccessCard: '#E1EFFA'
+};
+
+export { homePageBackGround, generalBackground };
