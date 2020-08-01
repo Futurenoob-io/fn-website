75a9db9499af63aced5cca18e6a8b345994b97e4
vippriya <vippriya@gmail.com>
2020-08-01 07:40:10 +0200

    WE1-46: card with image on the top updated

---
+++ src/baseComponents/cards/TopImageCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+	card: {
+		backgroundColor: 'halfwhite',
+		color: 'red',
+		padding: '1rem',
+		height: '4rem',
+		border: '1px solid #ccc',
+		border: '1px solid #ccc',
+		boxShadow: '2px 2px 6px 0px  rgba(0,0,0,0.3)'
+	},
+	cardImage: {
+		marginRight: '1em'
+	},
+	cardContent: {
+		marginRight: '1px'
+	},
+	cardTitle: {
+		margin: '0 0 .5em'
+	},
+	cardDescription: {
+		fontSize: '12px'
+	}
+});
+
+function TopImageCard({ id, imgSrc, imgAlt = '', title, description }) {
+	const classes = useStyles();
+
+	return (
+		<div key={id} className={classes.card}>
+			<img className={classes.cardImage} src={imgSr} alt={imgAlt} />
+			<div className={classes.cardBody}>
+				<h3 className={classes.cardTitle}>{title}</h3>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
+}
+export default TopImageCard;
