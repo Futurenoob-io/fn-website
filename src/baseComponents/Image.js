7c607eaccaa4f7f1a76107c583201549ffbcf294
vippriya <vippriya@gmail.com>
2020-08-01 15:06:08 +0200

    fn-web added a parameter for the image

--- src/baseComponents/Image.js
+++ src/baseComponents/Image.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Image({ src, description }) {
+function Image({ src, description, type }) {
 	return <img src={src} alt={description} />;
 }
 export default Image;
