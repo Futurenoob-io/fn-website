4a473cc18af4422da78ea7ae08657a5169731b24
vippriya <vippriya@gmail.com>
2020-08-01 07:39:50 +0200

    WE1-46: updated the media card

--- src/baseComponents/cards/MediaObject.js
+++ src/baseComponents/cards/MediaObject.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
-	media: {
+	card: {
 		display: 'flex',
 		alignItems: 'flexStart',
 		background: '#F6F3EB',
@@ -10,14 +10,20 @@ const useStyles = createUseStyles({
 		borderRadius: '3px',
 		maxWidth: '24em'
 	},
-	mediaObject: {
+	cardImage: {
 		marginRight: '1em'
 	},
-	mediaBody: {
+	cardContent: {
 		flex: 1
 	},
-	mediaHeading: {
+	cardTitle: {
 		margin: '0 0 .5em'
+	},
+	cardSubHeading: {
+		fontSize: '14px'
+	},
+	cardDescription: {
+		fontSize: '12px'
 	}
 });
 
@@ -25,12 +31,12 @@ function MediaObject({ id, imgSrc, imgAlt = '', heading, subHeading, description
 	const classes = useStyles();
 
 	return (
-		<div key={id} className={classes.media}>
-			<img className={classes.mediaObject} src={imgSr} alt={imgAlt} />
-			<div className={classes.mediaBody}>
-				<h3 className={classes.mediaHeading}>{heading}</h3>
-				<h3 className={classes.subHeading}>{subHeading}</h3>
-				<p>{description}</p>
+		<div key={id} className={classes.card}>
+			<img className={classes.cardImage} src={imgSr} alt={imgAlt} />
+			<div className={classes.cardContent}>
+				<h3 className={classes.cardTitle}>{heading}</h3>
+				<h3 className={classes.cardSubHeading}>{subHeading}</h3>
+				<p className={classes.cardDescription}>{description}</p>
 			</div>
 		</div>
 	);
